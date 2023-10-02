abc8aada5905ec0937cb6ce9fb406266d50d2022
Matthias Kargl <matthias-kargl@t-online.de>
2023-10-01 20:05:39 -0700

    use leassq function

--- handTracking.py
+++ handTracking.py
@@ -3,7 +3,7 @@ import mediapipe as mp
 import time
 import numpy as np
 import pandas as pd
-from scipy.optimize import fsolve
+import scipy
 
 
 class handDetector():
@@ -81,34 +81,78 @@ class handDetector():
         return img
 
     def calcZEgo(self, x, y):
-        yc,fl,pitch,zCam1,zCam2,zCam3,zCam4,zCam5 = x 
+        yc,fl,pitch,zCam1,zCam2,zCam3,zCam4,zCam5,zCam6,zCam7,zCam8,zCam9,zCam10 = x 
         eq1 = (np.sin(pitch) - (y - yc) / fl * np.cos(pitch)) * zCam1
         return eq1
     
-    def calcZEgoExt(self, y):
-            yc,fl,pitch,zCam1,zCam2,zCam3,zCam4,zCam5 = self.solutions
-            eq1 = (np.sin(pitch) - (y - yc) / fl * np.cos(pitch)) * zCam1
+    def calcZEgoExt(self, y, zCam):
+            yc,fl,pitch,zCam1,zCam2,zCam3,zCam4,zCam5,zCam6,zCam7,zCam8,zCam9,zCam10 = self.solutions
+            eq1 = (np.sin(pitch) - (y - yc) / fl * np.cos(pitch)) * zCam
             return eq1
 
     def calcZCam(self,x,y):
-        yc,fl,pitch,zCam1,zCam2,zCam3,zCam4,zCam5 = x 
+        yc,fl,pitch,zCam1,zCam2,zCam3,zCam4,zCam5,zCam6,zCam7,zCam8,zCam9,zCam10 = x 
         eq1 = self.zEgo / (np.sin(pitch) - (y - yc) / fl * np.cos(pitch))
         return eq1
 
 
     def functions(self,x):
-        yc,fl,pitch,zCam1,zCam2,zCam3,zCam4,zCam5 = x 
+        yc,fl,pitch,zCam1,zCam2,zCam3,zCam4,zCam5,zCam6,zCam7,zCam8,zCam9,zCam10 = x 
         eq1 = (np.sin(pitch) - (self.y1 - yc) / fl * np.cos(pitch)) * zCam1 - (np.sin(pitch) - (self.y2 - yc) / fl * np.cos(pitch)) * zCam2
         eq2 = (np.sin(pitch) - (self.y1 - yc) / fl * np.cos(pitch)) * zCam1 - (np.sin(pitch) - (self.y3 - yc) / fl * np.cos(pitch)) * zCam3
         eq3 = (np.sin(pitch) - (self.y1 - yc) / fl * np.cos(pitch)) * zCam1 - (np.sin(pitch) - (self.y4 - yc) / fl * np.cos(pitch)) * zCam4
         eq4 = (np.sin(pitch) - (self.y1 - yc) / fl * np.cos(pitch)) * zCam1 - (np.sin(pitch) - (self.y5 - yc) / fl * np.cos(pitch)) * zCam5
-        eq5 = (np.sin(pitch) - (self.y2 - yc) / fl * np.cos(pitch)) * zCam2 - (np.sin(pitch) - (self.y3 - yc) / fl * np.cos(pitch)) * zCam3
-        eq6 = (np.sin(pitch) - (self.y2 - yc) / fl * np.cos(pitch)) * zCam2 - (np.sin(pitch) - (self.y4 - yc) / fl * np.cos(pitch)) * zCam4
-        eq7 = (np.sin(pitch) - (self.y2 - yc) / fl * np.cos(pitch)) * zCam2 - (np.sin(pitch) - (self.y5 - yc) / fl * np.cos(pitch)) * zCam5
-        eq8 = (np.sin(pitch) - (self.y3 - yc) / fl * np.cos(pitch)) * zCam3 - (np.sin(pitch) - (self.y4 - yc) / fl * np.cos(pitch)) * zCam4
-        #eq9 = (np.sin(pitch) - (y3 - yc) / fl * np.cos(pitch)) * zCam3 - (np.sin(pitch) - (y5 - yc) / fl * np.cos(pitch)) * zCam5
-        #eq10 = (np.sin(pitch) - (y4 - yc) / fl * np.cos(pitch)) * zCam4 - (np.sin(pitch) - (y5 - yc) / fl * np.cos(pitch)) * zCam5
-        return [eq1, eq2, eq3, eq4,eq5,eq6,eq7,eq8]#,eq9,eq10]
+        eq5 = (np.sin(pitch) - (self.y1 - yc) / fl * np.cos(pitch)) * zCam1 - (np.sin(pitch) - (self.y6 - yc) / fl * np.cos(pitch)) * zCam6
+        eq6 = (np.sin(pitch) - (self.y1 - yc) / fl * np.cos(pitch)) * zCam1 - (np.sin(pitch) - (self.y7 - yc) / fl * np.cos(pitch)) * zCam7
+        eq7 = (np.sin(pitch) - (self.y1 - yc) / fl * np.cos(pitch)) * zCam1 - (np.sin(pitch) - (self.y8 - yc) / fl * np.cos(pitch)) * zCam8
+        eq8 = (np.sin(pitch) - (self.y1 - yc) / fl * np.cos(pitch)) * zCam1 - (np.sin(pitch) - (self.y9 - yc) / fl * np.cos(pitch)) * zCam9
+        eq9 = (np.sin(pitch) - (self.y1 - yc) / fl * np.cos(pitch)) * zCam1 - (np.sin(pitch) - (self.y10 - yc) / fl * np.cos(pitch)) * zCam10
+
+        eq10 = (np.sin(pitch) - (self.y2 - yc) / fl * np.cos(pitch)) * zCam2 - (np.sin(pitch) - (self.y3 - yc) / fl * np.cos(pitch)) * zCam3
+        eq11 = (np.sin(pitch) - (self.y2 - yc) / fl * np.cos(pitch)) * zCam2 - (np.sin(pitch) - (self.y4 - yc) / fl * np.cos(pitch)) * zCam4
+        eq12 = (np.sin(pitch) - (self.y2 - yc) / fl * np.cos(pitch)) * zCam2 - (np.sin(pitch) - (self.y5 - yc) / fl * np.cos(pitch)) * zCam5
+        eq13 = (np.sin(pitch) - (self.y2 - yc) / fl * np.cos(pitch)) * zCam2 - (np.sin(pitch) - (self.y6 - yc) / fl * np.cos(pitch)) * zCam6
+        eq14 = (np.sin(pitch) - (self.y2 - yc) / fl * np.cos(pitch)) * zCam2 - (np.sin(pitch) - (self.y7 - yc) / fl * np.cos(pitch)) * zCam7
+        eq15 = (np.sin(pitch) - (self.y2 - yc) / fl * np.cos(pitch)) * zCam2 - (np.sin(pitch) - (self.y8 - yc) / fl * np.cos(pitch)) * zCam8
+        eq16 = (np.sin(pitch) - (self.y2 - yc) / fl * np.cos(pitch)) * zCam2 - (np.sin(pitch) - (self.y9 - yc) / fl * np.cos(pitch)) * zCam9
+        eq17 = (np.sin(pitch) - (self.y2 - yc) / fl * np.cos(pitch)) * zCam2 - (np.sin(pitch) - (self.y10 - yc) / fl * np.cos(pitch)) * zCam10
+
+        eq18 = (np.sin(pitch) - (self.y3 - yc) / fl * np.cos(pitch)) * zCam3 - (np.sin(pitch) - (self.y4 - yc) / fl * np.cos(pitch)) * zCam4
+        eq19 = (np.sin(pitch) - (self.y3 - yc) / fl * np.cos(pitch)) * zCam3 - (np.sin(pitch) - (self.y5 - yc) / fl * np.cos(pitch)) * zCam5
+        eq20 = (np.sin(pitch) - (self.y3 - yc) / fl * np.cos(pitch)) * zCam3 - (np.sin(pitch) - (self.y6 - yc) / fl * np.cos(pitch)) * zCam6
+        eq21 = (np.sin(pitch) - (self.y3 - yc) / fl * np.cos(pitch)) * zCam3 - (np.sin(pitch) - (self.y7 - yc) / fl * np.cos(pitch)) * zCam7
+        eq22 = (np.sin(pitch) - (self.y3 - yc) / fl * np.cos(pitch)) * zCam3 - (np.sin(pitch) - (self.y8 - yc) / fl * np.cos(pitch)) * zCam8
+        eq23 = (np.sin(pitch) - (self.y3 - yc) / fl * np.cos(pitch)) * zCam3 - (np.sin(pitch) - (self.y9 - yc) / fl * np.cos(pitch)) * zCam9
+        eq24 = (np.sin(pitch) - (self.y3 - yc) / fl * np.cos(pitch)) * zCam3 - (np.sin(pitch) - (self.y10 - yc) / fl * np.cos(pitch)) * zCam10
+
+        eq25 = (np.sin(pitch) - (self.y4 - yc) / fl * np.cos(pitch)) * zCam4 - (np.sin(pitch) - (self.y5 - yc) / fl * np.cos(pitch)) * zCam5
+        eq26 = (np.sin(pitch) - (self.y4 - yc) / fl * np.cos(pitch)) * zCam4 - (np.sin(pitch) - (self.y6 - yc) / fl * np.cos(pitch)) * zCam6
+        eq27 = (np.sin(pitch) - (self.y4 - yc) / fl * np.cos(pitch)) * zCam4 - (np.sin(pitch) - (self.y7 - yc) / fl * np.cos(pitch)) * zCam7
+        eq28 = (np.sin(pitch) - (self.y4 - yc) / fl * np.cos(pitch)) * zCam4 - (np.sin(pitch) - (self.y8 - yc) / fl * np.cos(pitch)) * zCam8
+        eq29 = (np.sin(pitch) - (self.y4 - yc) / fl * np.cos(pitch)) * zCam4 - (np.sin(pitch) - (self.y9 - yc) / fl * np.cos(pitch)) * zCam9
+        eq30 = (np.sin(pitch) - (self.y4 - yc) / fl * np.cos(pitch)) * zCam4 - (np.sin(pitch) - (self.y10 - yc) / fl * np.cos(pitch)) * zCam10
+
+        eq31 = (np.sin(pitch) - (self.y5 - yc) / fl * np.cos(pitch)) * zCam5 - (np.sin(pitch) - (self.y6 - yc) / fl * np.cos(pitch)) * zCam6
+        eq32 = (np.sin(pitch) - (self.y5 - yc) / fl * np.cos(pitch)) * zCam5 - (np.sin(pitch) - (self.y7 - yc) / fl * np.cos(pitch)) * zCam7
+        eq33 = (np.sin(pitch) - (self.y5 - yc) / fl * np.cos(pitch)) * zCam5 - (np.sin(pitch) - (self.y8 - yc) / fl * np.cos(pitch)) * zCam8
+        eq34 = (np.sin(pitch) - (self.y5 - yc) / fl * np.cos(pitch)) * zCam5 - (np.sin(pitch) - (self.y9 - yc) / fl * np.cos(pitch)) * zCam9
+        eq35 = (np.sin(pitch) - (self.y5 - yc) / fl * np.cos(pitch)) * zCam5 - (np.sin(pitch) - (self.y10 - yc) / fl * np.cos(pitch)) * zCam10
+
+        eq36 = (np.sin(pitch) - (self.y6 - yc) / fl * np.cos(pitch)) * zCam6 - (np.sin(pitch) - (self.y7 - yc) / fl * np.cos(pitch)) * zCam7
+        eq37 = (np.sin(pitch) - (self.y6 - yc) / fl * np.cos(pitch)) * zCam6 - (np.sin(pitch) - (self.y8 - yc) / fl * np.cos(pitch)) * zCam8
+        eq38 = (np.sin(pitch) - (self.y6 - yc) / fl * np.cos(pitch)) * zCam6 - (np.sin(pitch) - (self.y9 - yc) / fl * np.cos(pitch)) * zCam9
+        eq39 = (np.sin(pitch) - (self.y6 - yc) / fl * np.cos(pitch)) * zCam6 - (np.sin(pitch) - (self.y10 - yc) / fl * np.cos(pitch)) * zCam10
+
+        eq40 = (np.sin(pitch) - (self.y7 - yc) / fl * np.cos(pitch)) * zCam7 - (np.sin(pitch) - (self.y8 - yc) / fl * np.cos(pitch)) * zCam8
+        eq41 = (np.sin(pitch) - (self.y7 - yc) / fl * np.cos(pitch)) * zCam7 - (np.sin(pitch) - (self.y9 - yc) / fl * np.cos(pitch)) * zCam9
+        eq42 = (np.sin(pitch) - (self.y7 - yc) / fl * np.cos(pitch)) * zCam7 - (np.sin(pitch) - (self.y10 - yc) / fl * np.cos(pitch)) * zCam10
+
+        eq43 = (np.sin(pitch) - (self.y8 - yc) / fl * np.cos(pitch)) * zCam8 - (np.sin(pitch) - (self.y9 - yc) / fl * np.cos(pitch)) * zCam9
+        eq44 = (np.sin(pitch) - (self.y8 - yc) / fl * np.cos(pitch)) * zCam8 - (np.sin(pitch) - (self.y10 - yc) / fl * np.cos(pitch)) * zCam10
+
+        eq45 = (np.sin(pitch) - (self.y9 - yc) / fl * np.cos(pitch)) * zCam9 - (np.sin(pitch) - (self.y10 - yc) / fl * np.cos(pitch)) * zCam10
+
+        return [eq1,eq2,eq3,eq4,eq5,eq6,eq7,eq8,eq9,eq10,eq11,eq12,eq13,eq14,eq15,eq16,eq17,eq18,eq19,eq20,eq21,eq22,eq23,eq24,eq25,eq26,eq27,eq28,eq29,eq30,eq31,eq32,eq33,eq34,eq35,eq36,eq37,eq38,eq39,eq40,eq41,eq42,eq43,eq44,eq45]
 
     def findCalibration(self, data : pd.DataFrame):
         vals = data.mean(axis=0)
@@ -121,17 +165,27 @@ class handDetector():
         self.y2 = self.y_vals['12.0yr']
         self.y3 = self.y_vals['16.0yr']
         self.y4 = self.y_vals['20.0yr']
+        self.y6 = self.y_vals['4.0yl']
+        self.y7 = self.y_vals['8.0yl']
+        self.y8 = self.y_vals['12.0yl']
+        self.y9 = self.y_vals['16.0yl']
+        self.y10 = self.y_vals['20.0yl']
         print(self.y_vals)
 
-        solutions= fsolve(self.functions, [300, 600,np.deg2rad(-30),30,30,30,30,33])
+        solutions= scipy.optimize.leastsq(self.functions, [300, 600,np.deg2rad(-10),30,30,30,30,33,30,30,30,30,33])[0]
         print('solution: ', solutions)
-        print('solution accuracy', self.functions(solutions))
+        #print('solution accuracy', self.functions(solutions))
         zEgo1 = self.calcZEgo(solutions, self.y1)
         zEgo2 = self.calcZEgo(solutions, self.y2)
         zEgo3 = self.calcZEgo(solutions, self.y3)
         zEgo4 = self.calcZEgo(solutions, self.y4)
         zEgo5 = self.calcZEgo(solutions, self.y5)
-        print('zEgos',zEgo1, zEgo2,zEgo3,zEgo4,zEgo5)
+        zEgo6 = self.calcZEgo(solutions, self.y6)
+        zEgo7 = self.calcZEgo(solutions, self.y7)
+        zEgo8 = self.calcZEgo(solutions, self.y8)
+        zEgo9 = self.calcZEgo(solutions, self.y9)
+        zEgo10 = self.calcZEgo(solutions, self.y10)
+        print('zEgos',zEgo1, zEgo2,zEgo3,zEgo4,zEgo5,zEgo6,zEgo7,zEgo8,zEgo9,zEgo10)
         self.solutions = solutions
         self.zEgo = zEgo1
 
@@ -160,7 +214,7 @@ class handDetector():
         return self.handDistFeatures[key]*self.solutions[1]/imgDist
     
     def calcImgXY(self, xEgo,zEgo,yEgo,xc):
-        yc,fl,pitch,zCam1,zCam2,zCam3,zCam4,zCam5 = self.solutions
+        yc,fl,pitch,zCam1,zCam2,zCam3,zCam4,zCam5,zCam6,zCam7,zCam8,zCam9,zCam10 = self.solutions
         yCam = (xEgo-zEgo/np.tan(pitch))/(np.cos(pitch)**2/np.sin(pitch)+np.sin(pitch))
         zCam = (zEgo + np.cos(pitch)*yCam)/np.sin(pitch)
 
@@ -270,10 +324,11 @@ def main():
                             dist = np.sqrt((tipArray[i,1]-tipArray[i+5,1])**2 +(tipArray[i,2]-tipArray[i+5,2])**2)
 
                             zCamFinger = hd.calcZCamFromFingerSize(key,dist)
-                            zEgo = hd.calcZEgoExt(zCamFinger)
-
+                            zEgo = hd.calcZEgoExt(tipArray[i,2],zCamFinger)
+                            cv2.putText(img,str(int(zEgo)),(int(tipArray[i,1]),int(tipArray[i,2])), cv2.FONT_HERSHEY_PLAIN,1, (255,255,255),3) #scale, color, thickness
+ 
                             if abs(zEgo - hd.zEgo) < 5:
-                                print(zEgo, hd.Ego, id, key)
+                                #print(zEgo, hd.zEgo, id, key)
                                 cv2.circle(img, (int(tipArray[i,1]),int(tipArray[i,2])), 25, (0,0,255), cv2.FILLED)
 
 
